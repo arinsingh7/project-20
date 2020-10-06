ac29ce7a5d2861f3c7c7d77caa2914a244488fd7
vidhibhati16 <71395900+vidhibhati16@users.noreply.github.com>
2020-10-07 00:21:08 +0530

    P20 - feedback 1

--- sketch.js
+++ sketch.js
@@ -11,8 +11,12 @@ function setup() {
   
   car = createSprite(50, 200, 50, 50);
   car.velocityX = speed;
+  
+  //give your car a colour
 
   wall = createSprite(1500, 200, 60, height/2);
+  
+  //give your wall a colour
 
 
 }
@@ -24,12 +28,19 @@ function draw() {
     car.velocityX = 0;
     var deformation = 0.5 * weight * speed * speed/22500;
     if(deformation>180) {
+      
+      //WRITE THE RGB CODES FOR COLOURS
+      //car.shapeColor=color(255,0,0);
       car.shapeColor = "red";
     }
     if(deformation<180 && deformation>100) {
+      
+      //car.shapeColor=color(230,230,0);
       car.shapeColor = "blue";
     }
     if(deformation<100) {
+      
+      //car.shapeColor=color(0,255,0);
       car.shapeColor = "green";
     }
 
